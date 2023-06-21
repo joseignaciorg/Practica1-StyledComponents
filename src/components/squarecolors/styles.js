eca8010530693b9554433045719db61eff6b529d
joseignaciorg <r.g.joseignacio@gmail.com>
2023-06-21 22:45:41 +0200

    marg

--- src/components/squarecolors/styles.js
+++ src/components/squarecolors/styles.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 const StyledDiv = styled.div`
     height:12.5rem; 
-    width:12.5rem; 
+    width:12.5rem;
+    margin-bottom :.25rem ;
     background-color:${props=>props.active ? 'red' : 'yellow'};
 `;
 
